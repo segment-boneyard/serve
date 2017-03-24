5b179ed2f7a3fab3930aec9eb025be5de7dd5013
Fouad Matin <m@fouad.co>
2017-03-24 11:06:28 -0700

    add not found redirect hook option

--- main.go
+++ main.go
@@ -4,18 +4,20 @@ import "github.com/segmentio/serve/logger"
 import "github.com/segmentio/go-log"
 import "github.com/tj/docopt"
 import "net/http"
+import "fmt"
 
 var Version = "1.1.0"
 
 const Usage = `
   Usage:
-    serve <dir> [--bind addr] [--prefix path]
+    serve <dir> [--bind addr] [--prefix path] [--error path]
     serve -h | --help
     serve --version
 
   Options:
     -p, --prefix path   url prefix [default: /]
     -b, --bind addr     bind address [default: 0.0.0.0:3000]
+    -e, --error path    path to serve on error [default: ]
     -h, --help          output help information
     -v, --version       output version
 
@@ -27,6 +29,7 @@ func main() {
 
 	prefix := args["--prefix"].(string)
 	addr := args["--bind"].(string)
+	errPage := args["--error"].(string)
 	dir := args["<dir>"].(string)
 
 	log.Info("binding to %s", addr)
@@ -34,8 +37,42 @@ func main() {
 
 	h := logger.New(http.StripPrefix(prefix, http.FileServer(http.Dir(dir))))
 
-	err = http.ListenAndServe(addr, h)
+	err = http.ListenAndServe(addr, NotFoundHook{h: h, ErrorPage: errPage})
 	if err != nil {
 		log.Error("failed to bind: %s", err)
 	}
+}
+
+type hookedResponseWriter struct {
+	http.ResponseWriter
+	Request   *http.Request
+	ignore    bool
+	ErrorPage string
+}
+
+func (hrw *hookedResponseWriter) WriteHeader(status int) {
+	fmt.Println(status)
+	fmt.Println("----" + hrw.ErrorPage + "-----")
+	if status == 404 && hrw.ErrorPage != "" {
+		hrw.ignore = true
+		http.Redirect(hrw.ResponseWriter, hrw.Request, hrw.ErrorPage, http.StatusSeeOther)
+	} else {
+		hrw.ResponseWriter.WriteHeader(status)
+	}
+}
+
+func (hrw *hookedResponseWriter) Write(p []byte) (int, error) {
+	if hrw.ignore {
+		return len(p), nil
+	}
+	return hrw.ResponseWriter.Write(p)
+}
+
+type NotFoundHook struct {
+	h         http.Handler
+	ErrorPage string
+}
+
+func (nfh NotFoundHook) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	nfh.h.ServeHTTP(&hookedResponseWriter{ResponseWriter: w, Request: r, ErrorPage: nfh.ErrorPage}, r)
 }
